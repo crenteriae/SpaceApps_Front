2af632c2974f767939de7224a7d9e41c2ee55108
Cesar <crenteriaejr@gmail.com>
2023-10-08 15:00:54 -0700

    updated styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from 'tailwindcss'
 const {nextui} = require("@nextui-org/react");
-
+import { Libre_Baskerville, Open_Sans } from 'next/font/google';
 
 const config: Config = {
   content: [
