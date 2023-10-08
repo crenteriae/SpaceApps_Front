bec5f1ba1442e52ebfd621b6787c013c3214e2ca
Rugroso <133253941+Rugroso@users.noreply.github.com>
2023-10-08 14:49:13 -0700

    Tierra datos

--- src/app/components/earth/index.js
+++ src/app/components/earth/index.js
@@ -1,8 +1,43 @@
 "use client";
 import { useState, useEffect, useRef } from 'react';
-import { Canvas, useLoader, useFrame } from '@react-three/fiber';
-import { TextureLoader, Vector3, Color, Backside } from 'three';
+import { Canvas, useLoader, useFrame, useThree } from '@react-three/fiber';
+import { TextureLoader, Vector3, Color } from 'three';
 import { OrbitControls, Html } from '@react-three/drei';
+
+const styles = {
+    container: {
+        backgroundColor: 'rgba(255,255,255,0.8)',
+        padding: '10px',
+        borderRadius: '5px',
+        width: '240px',     
+        maxHeight: '300px',  
+        overflow: 'auto',
+        boxShadow: '0px 0px 10px rgba(0,0,0,0.2)',  
+        fontSize: '14px'    
+    },
+    listItem: {
+        cursor: 'pointer',
+        marginBottom: '5px',
+        borderBottom: '1px solid rgba(0,0,0,0.1)',  
+        padding: '8px 0',    
+        transition: 'background-color 0.3s ease', 
+        "&:hover": {
+            backgroundColor: 'rgba(0,0,0,0.05)'  
+        }
+    }
+};
+function handleClickOnFirePoint(fire, camera, controls) {
+    const position = new Vector3();
+    position.setFromSphericalCoords(
+        2.5,
+        (Math.PI/2) - fire.latitude * (Math.PI/180),
+        (fire.longitude+90) * (Math.PI/180)
+    ).multiplyScalar(1.2); 
+    camera.position.lerp(position, 1);
+    controls.target.lerp(position, 1);
+    controls.update();
+}
+
 
 function FirePoint({ lat, long, info ,setHoverChange, hoverChange}) {
     const [hovered, setHovered] = useState(false);
@@ -32,7 +67,6 @@ function FirePoint({ lat, long, info ,setHoverChange, hoverChange}) {
         <>
         <mesh 
             position={position.toArray()} 
-            //onPointerOver={() => handleHover()} 
             onPointerEnter={() => handleHover()}
         >
             <sphereGeometry args={[0.005, 15, 32]} />
@@ -50,10 +84,10 @@ function FirePoint({ lat, long, info ,setHoverChange, hoverChange}) {
     );
 }
 
-function RotatingEarth({fireData}) {
+function RotatingEarth({ fireData }) {
     const meshRef = useRef();
     const texture = useLoader(TextureLoader, '/assets/EarthTexture.jpg');
-    const [hoverChange, setHoverChange] = useState(false); 
+    const [hoverChange, setHoverChange] = useState(false);
 
     useFrame(() => {
         if (meshRef.current) {
@@ -62,18 +96,29 @@ function RotatingEarth({fireData}) {
     });
 
     return (
-        <mesh ref={meshRef} scale={2}>
-            <sphereGeometry />
-            <meshStandardMaterial map={texture} />
-            {fireData.map((fire, index) => (
-                <FirePoint key={index} lat={fire.latitude} long={fire.longitude} info={fire.brightness.toString()} setHoverChange={setHoverChange}  hoverChange={hoverChange}/>
-            ))}
-        </mesh>
+        <>
+            <mesh ref={meshRef} scale={2}>
+                <sphereGeometry />
+                <meshStandardMaterial map={texture} />
+                {fireData.map((fire, index) => (
+                    <FirePoint 
+                        key={index} 
+                        lat={fire.latitude} 
+                        long={fire.longitude} 
+                        info={fire.brightness.toString()} 
+                        setHoverChange={setHoverChange}  
+                        hoverChange={hoverChange}
+                    />
+                ))}
+            </mesh>
+        </>
     );
 }
 
+
 export default function Earth() {
     const [fireData, setFireData] = useState([]);
+
 
     useEffect(() => {
         async function fetchData() {
@@ -83,36 +128,51 @@ export default function Earth() {
                     throw new Error('Respuesta no exitosa');
                 }
                 const data = await response.json();
-    
+
                 const randomData = [];
                 for (let i = 0; i < 20; i++) {
                     const randomIndex = Math.floor(Math.random() * data.length);
                     randomData.push(data[randomIndex]);
                 }
-    
+
                 setFireData(randomData);
             } catch (error) {
                 console.error("Error al obtener los datos:", error.message);
             }
         }
-    
+
         fetchData();
     }, []);
-    
 
     return (
-        <Canvas raycaster={{threshold: 0.5}}>
-            <ambientLight intensity={0.5} />
-            <pointLight intensity={80} position={[5, 0, 0]} />
-            <RotatingEarth fireData={fireData} />
-            <OrbitControls 
-                enableZoom={true} 
-                enableRotate={true} 
-                autoRotate={true}
-                autoRotateSpeed={0.5}
-                minDistance={2.5}
-                maxDistance={8}
-            />
-        </Canvas>
+        <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
+            <div style={{ ...styles.container, position: 'absolute', top: '10px', left: '10px', zIndex: 1 }}>
+                <ul>
+                    {fireData.map((fire, index) => (
+                        <li 
+                            key={index} 
+                            style={styles.listItem}
+                            onClick={() => handleClickOnFirePoint(fire, camera, controls)} 
+                        >
+                            Firewild detected in: Latitude: {fire.latitude.toFixed(2)}, Longitude: {fire.longitude.toFixed(2)}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <Canvas raycaster={{threshold: 0.5}}>
+                <ambientLight intensity={0.5} />
+                <pointLight intensity={80} position={[5, 0, 0]} />
+                <RotatingEarth fireData={fireData} />
+                <OrbitControls 
+                    enableZoom={true} 
+                    enableRotate={true} 
+                    autoRotate={true}
+                    autoRotateSpeed={0.5}
+                    minDistance={2.5}
+                    maxDistance={8}
+                />
+            </Canvas>
+        </div>
     );
 }
