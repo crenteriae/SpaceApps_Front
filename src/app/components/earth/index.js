2e22dd9774d136ae955b33e25cff94805e2f7f9a
Rugroso <133253941+Rugroso@users.noreply.github.com>
2023-10-08 03:09:26 -0700

    Earth

--- src/app/components/earth/index.js
+++ src/app/components/earth/index.js
@@ -4,7 +4,7 @@ import { Canvas, useLoader, useFrame } from '@react-three/fiber';
 import { TextureLoader, Vector3, Color, Backside } from 'three';
 import { OrbitControls, Html } from '@react-three/drei';
 
-function FirePoint({ lat, long, info }) {
+function FirePoint({ lat, long, info ,setHoverChange, hoverChange}) {
     const [hovered, setHovered] = useState(false);
     const position = new Vector3();
     const radius = 1.0;
@@ -13,19 +13,33 @@ function FirePoint({ lat, long, info }) {
         (Math.PI / 2) - lat * (Math.PI / 180),
         (long+90) * (Math.PI / 180)
     );
-
+    if(hovered && !hoverChange){
+        setHovered(false);
+    }
+    const handleHover = ()=>{
+        if(!hovered){
+            if(hoverChange){
+                setHoverChange(false); 
+                setHovered(true);   
+            }else{
+                setHoverChange(true);  
+                setHovered(true);
+            }
+        }
+    }
+    
     return (
         <>
         <mesh 
             position={position.toArray()} 
-            onPointerOver={() => setHovered(true)} 
-            onPointerOut={() => setHovered(false)} 
+            //onPointerOver={() => handleHover()} 
+            onPointerEnter={() => handleHover()}
         >
             <sphereGeometry args={[0.005, 15, 32]} />
             <meshPhongMaterial color={new Color('yellow')} emissive={new Color('yellow')} />
             {hovered && (
-                <Html distanceFactor={10}>
-                    <div style={{background: 'white', padding: '0.5em'}}>
+                <Html distanceFactor={3}>
+                    <div className='bg-white p-1 rounded-lg shadow-lg'>
                         {info}
                     </div>
                 </Html>
@@ -39,10 +53,11 @@ function FirePoint({ lat, long, info }) {
 function RotatingEarth({fireData}) {
     const meshRef = useRef();
     const texture = useLoader(TextureLoader, '/assets/EarthTexture.jpg');
+    const [hoverChange, setHoverChange] = useState(false); 
 
     useFrame(() => {
         if (meshRef.current) {
-            meshRef.current.rotation.y += 0.003;
+            meshRef.current.rotation.y += 0.000;
         }
     });
 
@@ -51,7 +66,7 @@ function RotatingEarth({fireData}) {
             <sphereGeometry />
             <meshStandardMaterial map={texture} />
             {fireData.map((fire, index) => (
-                <FirePoint key={index} lat={fire.latitude} long={fire.longitude} info={fire.brightness.toString()} />
+                <FirePoint key={index} lat={fire.latitude} long={fire.longitude} info={fire.brightness.toString()} setHoverChange={setHoverChange}  hoverChange={hoverChange}/>
             ))}
         </mesh>
     );
@@ -70,7 +85,7 @@ export default function Earth() {
                 const data = await response.json();
     
                 const randomData = [];
-                for (let i = 0; i < 100; i++) {
+                for (let i = 0; i < 20; i++) {
                     const randomIndex = Math.floor(Math.random() * data.length);
                     randomData.push(data[randomIndex]);
                 }
@@ -86,7 +101,7 @@ export default function Earth() {
     
 
     return (
-        <Canvas>
+        <Canvas raycaster={{threshold: 0.5}}>
             <ambientLight intensity={0.5} />
             <pointLight intensity={80} position={[5, 0, 0]} />
             <RotatingEarth fireData={fireData} />
@@ -95,8 +110,8 @@ export default function Earth() {
                 enableRotate={true} 
                 autoRotate={true}
                 autoRotateSpeed={0.5}
-                minDistance={3}
-                maxDistance={7}
+                minDistance={2.5}
+                maxDistance={8}
             />
         </Canvas>
     );
